437c7589da5c432658cf834d27b808676bec08ac
FFire <8497715@gmail.com>
2022-12-09 04:21:43 +0500

    fix: textclipp

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-transparent text-8xl p-8 text-center font-black font-sans bg-clip-text text-clip bg-gradient-to-r from-sky-600 to-rose-600">Tailwindcss is runing</h1>
+      <h1 className="text-transparent text-8xl p-8 text-center font-black font-sans bg-clip-text bg-gradient-to-r from-sky-600 to-rose-600">Tailwindcss is runing</h1>
     </>
   )
 }
