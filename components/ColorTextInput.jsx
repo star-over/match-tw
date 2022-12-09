bf4324160ea808134e352c19a05674576a2e49ce
FFire <8497715@gmail.com>
2022-12-10 02:31:13 +0500

    add distance calculator

--- components/ColorTextInput.jsx
+++ components/ColorTextInput.jsx
@@ -1,12 +1,26 @@
+import Color from "colorjs.io";
+
 import { useEffect, useState } from "react"
 
-export const ColorTextInput = () => {
+export const ColorTextInput = (props) => {
+  const { targetColor, setTargetColor } = props;
   const [textcolor, setTextColor] = useState("#123456");
-  useEffect(() => console.log({textcolor}), [textcolor]);
+  useEffect(() => console.log({targetColor}), [targetColor]);
+  const onChangeHandle = (e) => {
+    const { value } = e.target;
+    setTextColor(value);
+    try {
+      const parsedColor =  Color.parse(value);
+      setTargetColor(parsedColor);
+      console.log("🚀 > onChangeHandle > parsedColor", {parsedColor});
+    } catch (err) {
+      console.log("🚀 > onChangeHandle > err", {err});
+    }
+  }
   return (
     <>
       <form action="echo.htmlacademy.ru">
-        <input type="text" value={textcolor} onChange={(e) => setTextColor(e.target.value)} />
+        <input type="text" value={textcolor} onChange={onChangeHandle} />
       </form>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,40 @@
+import Color from "colorjs.io";
+import { useEffect, useState } from "react";
 import { ColorSpot } from "../components/ColorSpot";
 import { ColorTextInput } from "../components/ColorTextInput";
+import { colorThemeDefault } from "../data/colorThemeDefault";
 
 export default function Home() {
+
+  const [targetColor, setTargetColor] = useState(null);
+  const [colors, setColors] = useState([]);
+  const [distances, setDistances] = useState([]);
+
+  useEffect(() => {
+    setColors(colorThemeDefault);
+  }, []);
+
+  useEffect(() => {
+    const distancesToTarget = colors
+    .map((item) => {
+      const dE2000 = Color.deltaE(targetColor, item.color, "2000");
+
+      return { ...item, dE2000 };
+    })
+    .sort(({ dE2000: a }, { dE2000: b })=> a - b)
+    .slice(0, 6);
+    setDistances(distancesToTarget);
+
+  }, [targetColor]);
+
+  useEffect(()=> {
+    console.log({distances})
+  }, [distances])
   return (
     <>
       <h1 className="text-transparent text-8xl p-8 text-center font-black font-sans bg-clip-text bg-gradient-to-r from-sky-600 to-rose-600">Tailwindcss is runing</h1>
       <ColorSpot />
-      <ColorTextInput />
+      <ColorTextInput targetColor={targetColor} setTargetColor={setTargetColor} />
     </>
   )
 }
