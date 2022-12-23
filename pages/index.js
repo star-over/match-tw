f0b3088a939f2804230bed76e0011c5e2cf9a794
A1exandr Belan <8497715@gmail.com>
2022-12-24 02:17:31 +0500

    RadioButtons (#3)
    * libs install
    
    * fix: context first attempt
    
    * fix: small fixes
    
    * fix: color list with current color info
    
    * add: RadioButton component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Title } from "../components/Title";
 import { Breakpoints } from "../components/Breakpoints";
 import { ColorSpotList } from "../components/ColorSpotList";
 import { ColorTextInput } from "../components/ColorTextInput";
+import { RadioBottons } from "../components/RadioBottons";
 
 export default function Home() {
 
@@ -9,7 +10,11 @@ export default function Home() {
     <>
       <div className="container sm:m-auto pb-32  border border-solid border-red-300">
         <Title />
+        <RadioBottons
+          variants={ ["2000", "76", "Jz", "CMC", "ITP", "OK"] } />
         <ColorTextInput />
+        <RadioBottons
+          variants={ ["12", "9", "6", "3"] } />
         <ColorSpotList />
       </div>
       <Breakpoints />
