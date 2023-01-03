13d7fc70a65ccf6125dd86eff292bcd83a9df158
FFire <8497715@gmail.com>
2023-01-04 02:50:32 +0500

    add: gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import Head from "next/head"
+import Script from "next/script"
 import { StoreContext } from "../store/StoreContext"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (<>
+    <Script
+      id="gtm"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','G-E7DF1SB3B4');`}} />
     <Head>
       <title>Find a closest tailwind color</title>
       <meta name='description' content='Closest color to tailwind palette CSS' />
