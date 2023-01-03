e47ed24a75f8a90eab0d44c3aadb3aae48bb6e65
A1exandr Belan <8497715@gmail.com>
2023-01-04 02:51:46 +0500

    Logo (#7)
    * remove: large files
    
    * add: logo
    
    * add: gtm

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,12 +1,19 @@
-export const Title = () => (
-  <div className="">
-    <h1 className="
-        text-transparent mt-12 mb-4 text-center font-black bg-clip-text bg-gradient-to-r from-sky-600 to-rose-600
-        text-2xl sm:text-3xl lg:text-5xl
-        "
-    >Tailwind color matching</h1>
+import Image from "next/image";
+import logoImage from "../public/logo/main_logo.png"
 
-    <p className="text-sm text-center"
-    >Description</p>
+export const Title = () => (
+  <div >
+    <h1 className="sr-only">Tailwind color matching</h1>
+    <Image
+      className="mt-12 mb-4 mx-auto container"
+      src={logoImage}
+      alt="logo sign"
+      placeholder="blur"
+      />
+
+    <p
+    className="text-xs sm:text-sm px-4 mx-auto text-center max-w-lg">
+      Tailwind Colour Matching is a web-based tool for finding the nearest colour from the Tailwind palette. Different algorithms can be used to find the nearest colour, and although different algorithms give different results, the best tool will be your eyes.
+    </p>
   </div>
 );
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,35 @@
+import Head from "next/head"
+import Script from "next/script"
 import { StoreContext } from "../store/StoreContext"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
+  return (<>
+    <Script
+      id="gtm"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','G-E7DF1SB3B4');`}} />
+    <Head>
+      <title>Find a closest tailwind color</title>
+      <meta name='description' content='Closest color to tailwind palette CSS' />
+      <link rel="icon" href="/favicon/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="128x128" href="/favicon/favicon-128x128.png" />
+      <link rel="icon" type="image/png" sizes="196x196" href="/favicon/favicon-196x196.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+    </Head>
     <StoreContext>
       <Component { ...pageProps } />
     </StoreContext>
-  )
+  </>)
 }
 
 export default MyApp
