33bf5714991d7ddf02504446f1ed2dbe0fd6c690
FFire <8497715@gmail.com>
2024-03-17 23:32:46 +0700

    fix: gtm

--- app/layout.js
+++ app/layout.js
@@ -9,15 +9,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Script
-        id="gtm"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','G-E7DF1SB3B4');`}} />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-E7DF1SB3B4"></Script>
+      <Script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-E7DF1SB3B4');
+      </Script>
       <body>{children}</body>
     </html>
   );
