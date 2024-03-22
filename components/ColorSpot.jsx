6d2aeca0cfd5498de06c3e6b8756cd99eac99e98
FFire <8497715@gmail.com>
2024-03-22 23:35:42 +0700

    fix: style logic

--- components/ColorSpot.jsx
+++ components/ColorSpot.jsx
@@ -3,16 +3,16 @@ import { CopyButton } from "./copyButton";
 import { cn } from "@/lib/utils";
 
 export function ColorSpot({ colorName, colorHex, dE }) {
+  const isNeedBorder = (dE < 2.5);
+  const deFixed = dE.toFixed(2);
 
   return (
     <div
-      className={cn("text-md text-center rounded-xl h-32",
+      className={cn(
+        "text-md text-center rounded-xl h-32",
         "flex flex-col justify-center items-center transition-all",
-        "delay-100 duration-300",
-        " outline-1 -outline-offset-8",
-        {
-          ["outline"]: (dE < 2.5),
-        }
+        "delay-100 duration-300 outline-1 -outline-offset-8",
+        { "outline": isNeedBorder }
       )}
       style={getContrastStyle(colorHex)}
     >
@@ -21,7 +21,7 @@ export function ColorSpot({ colorName, colorHex, dE }) {
       <CopyButton text={colorHex}></CopyButton>
       <div>
         <span className="text-xs">dE:&nbsp;</span>
-        <span className="text-sm">{dE.toFixed(2)}</span>
+        <span className="text-sm">{deFixed}</span>
       </div>
 
     </div>
