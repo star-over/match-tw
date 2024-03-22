3c9026db5b308ae779b1bfe73189fdac993ecc02
FFire <8497715@gmail.com>
2024-03-22 23:29:05 +0700

    fix: selected border by dE

--- components/ColorSpot.jsx
+++ components/ColorSpot.jsx
@@ -1,14 +1,19 @@
 import { getContrastStyle } from "@/lib/colorUtil";
 import { CopyButton } from "./copyButton";
+import { cn } from "@/lib/utils";
 
 export function ColorSpot({ colorName, colorHex, dE }) {
 
   return (
     <div
-      className="text-md text-center rounded-xl h-32
-      flex flex-col justify-center items-center transition-all
-      delay-100 duration-300
-      outline outline-1 -outline-offset-8"
+      className={cn("text-md text-center rounded-xl h-32",
+        "flex flex-col justify-center items-center transition-all",
+        "delay-100 duration-300",
+        " outline-1 -outline-offset-8",
+        {
+          ["outline"]: (dE < 2.5),
+        }
+      )}
       style={getContrastStyle(colorHex)}
     >
 
