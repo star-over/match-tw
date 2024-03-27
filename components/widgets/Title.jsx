4ec3b7f7264b98e69ef94ec07cfddb9e44922ed9
FFire <8497715@gmail.com>
2024-03-27 23:49:03 +0700

    fix: title

--- components/widgets/Title.jsx
+++ components/widgets/Title.jsx
@@ -1,22 +1,22 @@
 import Image from "next/image";
 import logoImage from "@/public/logo/main_logo.png";
+import Link from "@/node_modules/next/link";
 
 export function Title() {
   return (
     <div className="container mt-8">
       <h1 className="sr-only">Tailwind colors matching</h1>
       <Image
-        className=""
         src={logoImage}
         alt="logo sign"
         placeholder="empty"
       />
 
-      <p
+      <h2
         className="text-xs sm:text-sm px-4 mx-auto text-center mt-4 text-balance">
-        Tailwind Colour Matching is a web-based tool that can help you find the closest color from the Tailwind palette.
-        We use different algorithms to find the nearest color, and although they may give different results, the best way to determine the closest color is to trust your eyes.
-      </p>
+        Tailwind Colour Matching is a web-based tool that can help you find the closest color from the <Link className="underline" href={"https://tailwindcss.com/docs/customizing-colors"}>Tailwind palette. </Link>
+        You can use different algorithms to find the nearest color, and although they may give different results. The best way to determine the closest color is to trust your eyes.
+      </h2>
     </div>
   );
 }
